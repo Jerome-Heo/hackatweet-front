db71dba55000f2f2b1a1a52ce180fa55a68ec20c
Robinson <robyvuillemin@gmail.com>
2024-07-19 10:46:29 +0200

    correction import antd

--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 import { configureStore } from '@reduxjs/toolkit';
 import user from '../reducers/user';
 import tweets from '../reducers/tweets';
-import 'antd/dist/antd.css';
+import 'antd/dist/reset.css';
 
 
 const store = configureStore({
