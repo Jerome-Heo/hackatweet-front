0930b4c4ee35f497c14f0db01e115bb96386d126
Robinson <robyvuillemin@gmail.com>
2024-07-19 13:34:57 +0200

    login page

--- components/Login.js
+++ components/Login.js
@@ -14,7 +14,7 @@ const URL = 'https://hackatweet-backend-ivory.vercel.app'
 const dispatch = useDispatch()
 
 const [isModalVisible, setModalVisible] = useState(false)
-
+const [isModalContent, setModalContent] = useState('Signin')
 const [signInUsername, setSignInUsername] = useState('')
 const [signInPassword, setSignInPassword] = useState('')
 const [signUpUsername, setSignUpUsername] = useState('')
@@ -73,6 +73,57 @@ const showModal = () => {
   setModalVisible(!isModalVisible)
 }
 
+const setModalIn = () => {
+  showModal()
+  setModalContent('Signin')
+}
+
+const setModalUp = () => {
+  showModal()
+  setModalContent('Signup')
+}
+
+
+let modalContent = isModalContent === "Signin" ?
+
+<div id="react-modals">
+
+<Modal getContainer="#react-modals" className={styles.modal} visible={isModalVisible} closable={true} onCancel={() => showModal()} footer={null} width={600} height={3}>
+
+<div className={styles.modalCont}>
+<img className={styles.logo} src="twitter_logo.png" alt="Hack a Tweet" width={55} height={45}/>
+
+<p className={styles.modalTitle}>Connect to Hackatweet {erreurIn && <p>user not found</p>}</p> {/* sagouin faudra que ça dégage*/}
+
+<input className={styles.input} type="text" placeholder="username" value={signInUsername} onChange={(e) => setSignInUsername(e.target.value)}/>
+<input className={styles.input} type="password" placeholder="password" value={signInPassword} onChange={(e) => setSignInPassword(e.target.value)}/>
+<button className={styles.inputBtn} onClick={() => handleSignIn()}>Sign in</button>
+</div>
+
+</Modal>
+</div> : 
+
+<div id="react-modals">
+  <Modal getContainer="#react-modals" className={styles.modal} visible={isModalVisible} closable={true} onCancel={() => showModal()} footer={null} width={600} height={500}>
+
+<div className={styles.modalCont}>
+<img className={styles.logo} src="twitter_logo.png" alt="Hack a Tweet" width={55} height={45}/>
+
+<p className={styles.modalTitle}>Create your Hackatweet account {erreurUP && <p>y'a eu une erreur</p>}</p> {/* sagouin faudra que ça dégage*/}
+
+<input className={styles.input} type="text" placeholder="name" value={signUpName} onChange={(e) => setSignUpName(e.target.value)} />
+<input className={styles.input} type="text" placeholder="username" value={signUpUsername} onChange={(e) => setSignUpUsername(e.target.value)}/>
+<input className={styles.input} type="password" placeholder="password" value={signUpPassword} onChange={(e) => setSignUpPassword(e.target.value)}/>
+<button className={styles.inputBtn} onClick={() => handleSignUp()}>Sign up</button>
+</div>
+
+</Modal>
+</div>
+
+
+
+
+
   return (
     <div>
       <main className={styles.main}>
@@ -98,34 +149,11 @@ const showModal = () => {
           <h1 className={styles.title}>happening</h1>
           <h2 className={styles.titleBot}>Join Hackatweet today.</h2>
 
-          <button className={styles.SignUp} onClick={() => showModal()}>Sign up</button>
+          <button className={styles.SignUp} onClick={() => setModalUp()}>Sign up</button>
           <p>Already have an account ?</p>
-          <button className={styles.SignIn} onClick={() => showModal()}>Sign in</button>
+          <button className={styles.SignIn} onClick={() => setModalIn()}>Sign in</button>
           
-          {isModalVisible && <div id="react-modals">
-				<Modal getContainer="#react-modals" className={styles.modal} visible={isModalVisible} closable={false} footer={null}>
-            <button onClick={() => showModal()}>X</button>
-        <div className={styles.contSignin}>
-          
-
-            <p>Sign In {erreurIn && <p>user not found</p>}</p> {/* sagouin faudra que ça dégage*/}
-
-            <input type="text" placeholder="username" value={signInUsername} onChange={(e) => setSignInUsername(e.target.value)}/>
-            <input type="password" placeholder="password" value={signInPassword} onChange={(e) => setSignInPassword(e.target.value)}/>
-            <button onClick={() => handleSignIn()}>sign in</button>
-        </div>
-              
-        <div className={styles.contSignup}>
-
-            <p>Sign Up {erreurUP && <p>y'a eu une erreur</p>}</p> {/* sagouin faudra que ça dégage*/}
-
-            <input type="text" placeholder="name" value={signUpName} onChange={(e) => setSignUpName(e.target.value)} />
-            <input type="text" placeholder="username" value={signUpUsername} onChange={(e) => setSignUpUsername(e.target.value)}/>
-            <input type="password" placeholder="password" value={signUpPassword} onChange={(e) => setSignUpPassword(e.target.value)}/>
-            <button onClick={() => handleSignUp()}>sign up</button>
-            </div>
-          </Modal>
-        </div>}
+          {isModalVisible && modalContent}
 
 
        
