11e43a39b5b88a8dac4c74d42702c3a27b75833c
jérôme <jplusquellec@gmail.com>
2024-08-20 16:53:55 +0200

    url vercel ok

--- components/Hashtag.js
+++ components/Hashtag.js
@@ -30,7 +30,7 @@ function Hashtag() {
 
     setQuery('#' + hashtag);
 
-    fetch(`https://https://hackatweet-front-phi.vercel.app/${user.token}/${hashtag}`)
+    fetch(`https://hackatweet-front-phi.vercel.app/${user.token}/${hashtag}`)
       .then(response => response.json())
       .then(data => {
         data.result && dispatch(loadTweets(data.tweets));
